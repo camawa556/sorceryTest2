c442f014903dc69185d37bb6b978fa511f6b6f9e
camawa556 <12e1407k@gmail.com>
2021-09-10 12:11:53 +0900

    finish_sorcery_4

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   def not_authenticated
     redirect_to login_path, alert: "Please login first"
   end
-  
+
   def login_required
     redirect_to login_url unless current_user
   end
